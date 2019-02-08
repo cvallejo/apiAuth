94a30486b04baf7ea69035157f0dace0190791d8
Claudio Vallejo <claudio.vallejo@gmail.com>
2019-02-08 12:48:03 -0300

    agrego sistema de notificacion con queue y redis

--- app/Notifications/SignupActivate.php
+++ app/Notifications/SignupActivate.php
@@ -5,8 +5,9 @@ namespace App\Notifications;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SignupActivate extends Notification
+class SignupActivate extends Notification implements ShouldQueue
 {
     use Queueable;
 
